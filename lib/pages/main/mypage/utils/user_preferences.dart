26f93f9472137ccf9dcc93455a262ef85f59e2b4
latteishorse <moots_photo@naver.com>
2022-07-12 13:32:21 +0900

    #6 User Profile(Mypage) 제작 (DB 연동 전)

---
+++ lib/pages/main/mypage/utils/user_preferences.dart
@@ -0,0 +1,21 @@
+import '../model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmy_tAYTaI_s0_x_0CItXgE7khwKORXSzXHg&usqp=CAU',
+    name: '닉네임',
+    email: '한줄 소개 입력',
+    about:
+        'Rick and Morty is an American adult animated science fiction sitcom created by Justin Roiland and Dan Harmon for Cartoon Networks nighttime programming block Adult Swim. The series follows the misadventures of cynical mad scientist Rick Sanchez and his good-hearted but fretful grandson Morty Smith, who split their time between domestic life and interdimensional adventures.',
+    image1:
+        'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/f2bc44a9d75daf5b21e45cde9e96808ea899ee4bee518cb1fa41fe894eff0cbc._V_SX1080_.png',
+    image2:
+        'https://observer.com/wp-content/uploads/sites/2/2021/09/rick-and-morty-season-5-evil-morty-return-1281723.jpeg?quality=80&w=635',
+    image3:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4WkYDEssEAgV1yTFcBr_A2m-ybF-HH7U0jg&usqp=CAU',
+    tag1: 'he11o world',
+    tag2: 'he22o world',
+    tag3: 'he33o world',
+  );
+}
