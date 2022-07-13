16154ac4a1fab2efaa39f2a82a7c8cbf1376be9c
latteishorse <moots_photo@naver.com>
2022-07-13 23:58:20 +0900

    #14 데모 준비

--- lib/pages/main/sharing/screens/tookPage.dart
+++ lib/pages/main/sharing/screens/tookPage.dart
@@ -10,16 +10,17 @@ class TookPage extends StatefulWidget {
 }
 
 class _TookPageState extends State<TookPage> {
-  getData() async{
-    var result = await http.get(Uri.parse('http://34.64.217.3:3000/ping'));
-    print(jsonDecode(result.body));
-  }
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    getData();
-  }
+  // getData() async {
+  //   var result = await http.get(Uri.parse('http://34.64.217.3:3000/ping'));
+  //   print(jsonDecode(result.body));
+  // }
+
+  // @override
+  // void initState() {
+  //   // TODO: implement initState
+  //   super.initState();
+  //   getData();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -34,16 +35,14 @@ class _TookPageState extends State<TookPage> {
                     generateCard(),
                     generateBlank(),
                   ],
-                )
-            )
-        )
-    );
+                ))));
   }
 }
 
 generateBlank() {
   return Container(
-    height: 160, color: Colors.white,
+    height: 250,
+    color: Colors.white,
     child: ListTile(),
   );
 }
@@ -63,7 +62,7 @@ generateBlank() {
 //   );
 // }
 
-generateCard(){
+generateCard() {
   return Container(
     margin: EdgeInsets.all(8.0),
     // decoration: BoxDecoration(border: Border.all(color: Colors.grey.shade300)),
@@ -81,19 +80,20 @@ generateCard(){
                 topLeft: Radius.circular(8.0),
                 topRight: Radius.circular(8.0),
               ),
-              child: Image.network("https://s3.us-west-2.amazonaws.com/secure.notion-static.com/234c130e-99c3-46ee-9f7e-df52627f5805/KakaoTalk_Photo_2022-07-04-10-44-49.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220713%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220713T120807Z&X-Amz-Expires=86400&X-Amz-Signature=0c0e9e00fdd1f9641333760e36aa428db3191d2b10681805fc6462963f7c4e4b&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22KakaoTalk_Photo_2022-07-04-10-44-49.gif%22&x-id=GetObject",
-                  width: 300, height: 250, fit: BoxFit.fitWidth),
+              child: Image.network(
+                  "http://34.64.217.3:3000/static/gonigoni.gif",
+                  width: 300,
+                  height: 250,
+                  fit: BoxFit.fitWidth),
             ),
             ListTile(
               tileColor: Color(0xc7ffffff),
               dense: true,
               visualDensity: VisualDensity(vertical: 3),
               title: Text("고니고니",
-                  style: TextStyle(
-                      fontSize: 40, fontWeight: FontWeight.bold)),
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
               subtitle: Text("캣홀릭 오타쿠",
-                  style: TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.w300)),
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300)),
             ),
           ],
         ),
