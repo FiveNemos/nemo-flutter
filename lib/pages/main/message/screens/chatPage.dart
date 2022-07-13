44f00ccc5785a5cc872860955a31bc45a8a2e0fe
latteishorse <moots_photo@naver.com>
2022-07-13 22:12:34 +0900

    #6 프로필 및 시연사항 수정

--- lib/pages/main/message/screens/chatPage.dart
+++ lib/pages/main/message/screens/chatPage.dart
@@ -10,37 +10,89 @@ class ChatPage extends StatefulWidget {
 
 class _ChatPageState extends State<ChatPage> {
   List<ChatUsers> original_chatUsers = [
-    ChatUsers(text: "고니고니", secondaryText: "캣홀릭 오타쿠", image: "https://ca.slack-edge.com/T01FZU4LB4Y-U038SKSQH0D-5494a00d9938-512", time: "Now"),
-    ChatUsers(text: "Opjoobe", secondaryText: "Ball is Life", image: "https://ca.slack-edge.com/T01FZU4LB4Y-U038VG8TD5H-2cdff7f10831-512", time: "3h"),
-    ChatUsers(text: "Jessy", secondaryText: "Just Dance", image: "https://ca.slack-edge.com/T01FZU4LB4Y-U038NSLLYUE-61acb0b06e68-512", time: "5h"),
-    ChatUsers(text: "Jocy", secondaryText: "Coffee Coffee", image: "https://ca.slack-edge.com/T01FZU4LB4Y-U038NSL7XAA-29c3e7232b89-512", time: "8 Jul"),
-    ChatUsers(text: "Chani", secondaryText: "PingPong is Life", image: "https://ca.slack-edge.com/T01FZU4LB4Y-U038XSFUSMS-5562753ddde3-512", time: "6 Jul"),
-    ChatUsers(text: "Krafton", secondaryText: "니 인생은 너꺼야!", image: "https://ca.slack-edge.com/T01FZU4LB4Y-U01GQQQGCBX-64f84f2f3f49-512", time: "30 Jun"),
-    ChatUsers(text: "Sanori", secondaryText: "정글러버", image: "https://ca.slack-edge.com/T01FZU4LB4Y-U024206FLQM-239a518483a5-512", time: "28 Jun"),
-    ChatUsers(text: "Sparta", secondaryText: "TEAM SPARTA", image: "https://ca.slack-edge.com/T01FZU4LB4Y-U01F70TFLCV-c33569d65252-512", time: "2 Jun"),
+    ChatUsers(
+        text: "고니고니",
+        secondaryText: "캣홀릭 오타쿠",
+        image:
+            "https://ca.slack-edge.com/T01FZU4LB4Y-U038SKSQH0D-5494a00d9938-512",
+        time: "Now"),
+    ChatUsers(
+        text: "정글러버",
+        secondaryText: "캣홀릭",
+        image:
+            "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/668a3935-3fcd-4bd5-a419-4cb7732a5288/KakaoTalk_Photo_2022-07-04-01-01-40.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220712%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220712T140031Z&X-Amz-Expires=86400&X-Amz-Signature=a9a591296e62e42bcb75d5a32f4082bd6e07239b04e544de79f36390268f7be8&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22KakaoTalk_Photo_2022-07-04-01-01-40.gif%22&x-id=GetObject",
+        time: "Now"),
+    ChatUsers(
+        text: "Opjoobe",
+        secondaryText: "Ball is Life",
+        image:
+            "https://ca.slack-edge.com/T01FZU4LB4Y-U038VG8TD5H-2cdff7f10831-512",
+        time: "3h"),
+    ChatUsers(
+        text: "Jessy",
+        secondaryText: "Just Dance",
+        image:
+            "https://ca.slack-edge.com/T01FZU4LB4Y-U038NSLLYUE-61acb0b06e68-512",
+        time: "5h"),
+    ChatUsers(
+        text: "Jocy",
+        secondaryText: "Coffee Coffee",
+        image:
+            "https://ca.slack-edge.com/T01FZU4LB4Y-U038NSL7XAA-29c3e7232b89-512",
+        time: "8 Jul"),
+    ChatUsers(
+        text: "Chani",
+        secondaryText: "PingPong is Life",
+        image:
+            "https://ca.slack-edge.com/T01FZU4LB4Y-U038XSFUSMS-5562753ddde3-512",
+        time: "6 Jul"),
+    ChatUsers(
+        text: "Krafton",
+        secondaryText: "니 인생은 너꺼야!",
+        image:
+            "https://ca.slack-edge.com/T01FZU4LB4Y-U01GQQQGCBX-64f84f2f3f49-512",
+        time: "30 Jun"),
+    ChatUsers(
+        text: "Sanori",
+        secondaryText: "정글러버",
+        image:
+            "https://ca.slack-edge.com/T01FZU4LB4Y-U024206FLQM-239a518483a5-512",
+        time: "28 Jun"),
+    ChatUsers(
+        text: "Sparta",
+        secondaryText: "TEAM SPARTA",
+        image:
+            "https://ca.slack-edge.com/T01FZU4LB4Y-U01F70TFLCV-c33569d65252-512",
+        time: "2 Jun"),
   ]; // 이걸 DB에서 받아오는거로 바꾸면 될듯
 
   List chatUsers = [];
-  resetConversation(){
-    setState((){chatUsers = original_chatUsers;});
+  resetConversation() {
+    setState(() {
+      chatUsers = original_chatUsers;
+    });
   }
 
-  searchConversation(text){
+  searchConversation(text) {
     var temp = [];
-    original_chatUsers.forEach((e){
-      if(e.text.startsWith(text)){
+    original_chatUsers.forEach((e) {
+      if (e.text.startsWith(text)) {
         temp.add(e);
       }
     });
-    setState((){chatUsers = temp;});
+    setState(() {
+      chatUsers = temp;
+    });
   }
+
   @override
   void initState() {
     super.initState();
     resetConversation();
   }
+
   @override
-  Widget buildSuggestions(BuildContext context){
+  Widget buildSuggestions(BuildContext context) {
     List<String> suggestions = [
       'Brazil',
       'China',
@@ -49,21 +101,21 @@ class _ChatPageState extends State<ChatPage> {
       'USA',
     ];
     return ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index){
-          final suggestion = suggestions[index];
-          return ListTile(
-            title : Text(suggestion),
-            onTap: (){
-            },
-          );
-        },
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        final suggestion = suggestions[index];
+        return ListTile(
+          title: Text(suggestion),
+          onTap: () {},
+        );
+      },
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
+      onTap: () {
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
@@ -74,13 +126,18 @@ class _ChatPageState extends State<ChatPage> {
             children: <Widget>[
               SafeArea(
                 child: Padding(
-                  padding: EdgeInsets.only(left: 16,right: 16,top: 10),
+                  padding: EdgeInsets.only(left: 16, right: 16, top: 10),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Text("Conversations",style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+                      Text(
+                        "Conversations",
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
                       Container(
-                        padding: EdgeInsets.only(left: 8,right: 8,top: 2,bottom: 2),
+                        padding: EdgeInsets.only(
+                            left: 8, right: 8, top: 2, bottom: 2),
                         height: 30,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(30),
@@ -88,9 +145,19 @@ class _ChatPageState extends State<ChatPage> {
                         ),
                         child: Row(
                           children: <Widget>[
-                            Icon(Icons.add, color: Colors.pink, size: 20,),
-                            SizedBox(width: 2,),
-                            Text("Add New",style: TextStyle(fontSize: 14,fontWeight: FontWeight.w800),),
+                            Icon(
+                              Icons.add,
+                              color: Colors.pink,
+                              size: 20,
+                            ),
+                            SizedBox(
+                              width: 2,
+                            ),
+                            Text(
+                              "Add New",
+                              style: TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.w800),
+                            ),
                           ],
                         ),
                       )
@@ -99,35 +166,32 @@ class _ChatPageState extends State<ChatPage> {
                 ),
               ),
               Padding(
-                padding: EdgeInsets.only(top: 16,left: 16,right: 16),
+                padding: EdgeInsets.only(top: 16, left: 16, right: 16),
                 child: TextField(
-                  onChanged: (text){
-                    if (text.isNotEmpty){
+                  onChanged: (text) {
+                    if (text.isNotEmpty) {
                       searchConversation(text);
-                    }
-                    else{
+                    } else {
                       resetConversation();
                     }
-                    },
+                  },
                   decoration: InputDecoration(
                     hintText: "Search...",
                     hintStyle: TextStyle(color: Colors.grey.shade600),
-                    prefixIcon: Icon(Icons.search,color: Colors.grey.shade600, size: 20,),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.grey.shade600,
+                      size: 20,
+                    ),
                     filled: true,
                     fillColor: Colors.grey.shade100,
                     contentPadding: EdgeInsets.all(8),
                     enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide(
-                            color: Colors.grey.shade100
-                        )
-                    ),
+                        borderSide: BorderSide(color: Colors.grey.shade100)),
                     focusedBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide(
-                            color: Colors.grey.shade100
-                        )
-                    ),
+                        borderSide: BorderSide(color: Colors.grey.shade100)),
                   ),
                 ),
               ),
@@ -136,13 +200,13 @@ class _ChatPageState extends State<ChatPage> {
                 shrinkWrap: true,
                 padding: EdgeInsets.only(top: 16),
                 physics: NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index){
+                itemBuilder: (context, index) {
                   return ConversationList(
                     name: chatUsers[index].text,
                     messageText: chatUsers[index].secondaryText,
                     imageUrl: chatUsers[index].image,
                     time: chatUsers[index].time,
-                    isMessageRead: (index == 0 || index == 3)?true:false,
+                    isMessageRead: (index == 0 || index == 3) ? true : false,
                   );
                 },
               ),
