b6e9b655f989a60e6b4e84d1a861c1a2fd839781
Chanik Kang <moots_photo@naver.com>
2022-07-13 21:30:51 +0900

    #6 프로필 NavBar 삭제

--- lib/pages/main/mypage/mypage.dart
+++ lib/pages/main/mypage/mypage.dart
@@ -20,47 +20,9 @@ class _MypagePageState extends State<MypagePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('마이페이지'),
+        title: Text('Profile'),
       ),
       body: MyProfile(),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.contacts),
-            label: '연락처',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.share),
-            label: '공유',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.message),
-            label: '메시지',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: '마이페이지',
-          ),
-        ],
-        currentIndex: 3,
-        fixedColor: Colors.blue,
-        onTap: (int index) {
-          switch (index) {
-            case 0:
-              Navigator.pushNamed(context, '/contacts');
-              break;
-            case 1:
-              Navigator.pushNamed(context, '/sharing');
-              break;
-            case 2:
-              Navigator.pushNamed(context, '/message');
-              break;
-            case 3:
-              Navigator.pushNamed(context, '/mypage');
-              break;
-          }
-        },
-      ),
     );
   }
 }
