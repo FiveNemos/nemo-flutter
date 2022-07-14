8deae4bbe68d1ce21d69185959455901b5fc9f2f
opjoobe <opjoobe@gmail.com>
2022-07-14 23:14:02 +0900

    #15 fix: 빌드오류 수정 및 폴더구조 개선

---
+++ lib/providers/message/message_store.dart
@@ -0,0 +1,56 @@
+// TEMP // FOR PROVIDER // 쓰려면 main.dart에 MultiProvider 적용해야함
+import '../../screens/message/message.dart';
+import '../../models/message/message_chatMessageModel.dart';
+
+class StoreMessage extends MessagePage {
+  Map messages_db = {
+    "Sanori": [
+      ChatMessage(messageContent: "안녕하세요", messageType: "receiver"),
+      ChatMessage(messageContent: "잘하고 계신가요 ㅎㅎ ", messageType: "receiver"),
+      ChatMessage(
+          messageContent: "앗 코치님! 플러터 하고 있어요. 금방 하겠죠..? ",
+          messageType: "sender"),
+      ChatMessage(messageContent: "예... 뭐 그럴수도 있겠다", messageType: "receiver"),
+      ChatMessage(messageContent: "감... 감사합니다^^", messageType: "sender"),
+    ],
+    "Opjoobe": [
+      ChatMessage(messageContent: "안녕하세요 주형님", messageType: "sender"),
+      ChatMessage(
+          messageContent: "앗 리더님! 친히 연락을 다 주시다니요", messageType: "receiver"),
+      ChatMessage(messageContent: "농구 한판 하실래요 ?", messageType: "receiver"),
+    ],
+    "Jocy": [
+      ChatMessage(messageContent: "안녕하세요 현욱님", messageType: "sender"),
+      ChatMessage(messageContent: "네 안녕하세요~", messageType: "receiver"),
+      ChatMessage(messageContent: "커피 한잔 하실래요 ?", messageType: "receiver"),
+    ],
+    "Jessy": [
+      ChatMessage(messageContent: "안녕하세요 현주님", messageType: "sender"),
+      ChatMessage(messageContent: "일어나셨나요 ?", messageType: "sender"),
+      ChatMessage(messageContent: "현주님.....?", messageType: "sender"),
+      ChatMessage(messageContent: "님..?", messageType: "sender"),
+    ],
+    "Chani": [
+      ChatMessage(messageContent: "안녕하세요 찬익님", messageType: "sender"),
+      ChatMessage(messageContent: "블루베리 가자구요?", messageType: "receiver"),
+      ChatMessage(messageContent: "좋~죠~", messageType: "sender"),
+    ],
+    "Krafton": [
+      ChatMessage(
+          messageContent: "안녕하세요 의장님:) 저희 발표 혹시..", messageType: "sender"),
+      ChatMessage(messageContent: "내가 말했지", messageType: "receiver"),
+      ChatMessage(
+          messageContent: "니 인생은 너꺼야!!! 운영진 신경쓰지마!!", messageType: "receiver"),
+      ChatMessage(messageContent: "알겠어?!!", messageType: "receiver"),
+    ],
+    "고니고니": [
+      ChatMessage(messageContent: "의장님께 채팅보내보기", messageType: "sender"),
+      ChatMessage(messageContent: "계란 2주내로 먹기", messageType: "sender"),
+    ],
+    "Sparta": [
+      ChatMessage(messageContent: "안녕하세요 성곤님", messageType: "receiver"),
+      ChatMessage(messageContent: "대표님 안녕하세요!", messageType: "sender"),
+      ChatMessage(messageContent: "디스 이즈 스파르타 !!!", messageType: "receiver"),
+    ],
+  };
+}
---
+++ lib/screens/mypage/mypage_profile_page.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../models/mypage/mypage_user.dart';
+import '../../tests/mypage/mypage_preferences.dart';
+import '../../widgets/mypage/mypage_profile_widget.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    final user = UserPreferences.myUser;
+
+    return Scaffold(
+      body: ListView(
+        physics: BouncingScrollPhysics(),
+        children: [
+          ProfileWidget(
+            imagePath: user.imagePath,
+            onClicked: () async {},
+          ),
+          const SizedBox(height: 20),
+          buildName(user),
+          const SizedBox(height: 20),
+          buildImage(user),
+          const SizedBox(height: 12),
+          buildTag(user),
+          const SizedBox(height: 24),
+          buildAbout(user),
+        ],
+      ),
+    );
+  }
+
+  Widget buildName(User user) => Column(
+        children: [
+          Text(
+            user.nickname,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            user.introduction,
+            style: TextStyle(color: Colors.grey, fontSize: 20),
+          )
+        ],
+      );
+
+  Widget buildAbout(User user) => Container(
+        padding: EdgeInsets.symmetric(horizontal: 48),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              user.title,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              user.about,
+              style: TextStyle(fontSize: 20, height: 1.4),
+            ),
+          ],
+        ),
+      );
+}
+
+Widget buildTag(User user) => Container(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            width: 120,
+            height: 40,
+            child: Text(
+              user.tag1,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            width: 120,
+            height: 40,
+            child: Text(
+              user.tag2,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(
+            width: 120,
+            height: 40,
+            child: Text(
+              user.tag3,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
+
+Widget buildImage(User user) => Container(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(
+              user.image1,
+              width: 120,
+              height: 120,
+              fit: BoxFit.cover,
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(
+              user.image2,
+              width: 120,
+              height: 120,
+              fit: BoxFit.cover,
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.network(
+              user.image3,
+              fit: BoxFit.cover,
+              width: 120,
+              height: 120,
+            ),
+          ),
+        ],
+      ),
+    );
