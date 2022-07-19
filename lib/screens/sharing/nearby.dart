40b2ae9399790212ae6bffc267e58967cafaaa42
latteishorse <moots_photo@naver.com>
2022-07-19 15:03:38 +0900

    #23 nearby automation 3/4

--- lib/screens/sharing/nearby.dart
+++ lib/screens/sharing/nearby.dart
@@ -216,99 +216,45 @@ class _MyBodyState extends State<NearbyConnection> {
   /// Called upon Connection request (on both devices)
   /// Both need to accept connection to start sending/receiving
   void onConnectionInit(String id, ConnectionInfo info) {
-    showModalBottomSheet(
-      context: context,
-      builder: (builder) {
-        return Center(
-          child: Column(
-            children: <Widget>[
-              ElevatedButton(
-                child: Text("Accept Connection"),
-                onPressed: () {
-                  Navigator.pop(context);
-                  setState(() {
-                    endpointMap[id] = info;
-                  });
-                  Nearby().acceptConnection(
-                    id,
-                    onPayLoadRecieved: (endid, payload) async {
-                      if (payload.type == PayloadType.BYTES) {
-                        String b = "https://swjungle.net";
+    setState(() {
+      endpointMap[id] = info;
+    });
+    Nearby().acceptConnection(
+      id,
+      onPayLoadRecieved: (endid, payload) async {
+        if (payload.type == PayloadType.BYTES) {
+          String b = "https://swjungle.net";
 
-                        final url = Uri.parse(b);
+          final url = Uri.parse(b);
 
-                        if (await canLaunchUrl(url)) {
-                          launchUrl(url);
-                        } else {
-                          print('Could not launch $url');
-                        }
+          if (await canLaunchUrl(url)) {
+            launchUrl(url);
+          } else {
+            print('Could not launch $url');
+          }
 
-                        String str = String.fromCharCodes(payload.bytes!);
-                        // showSnackbar(endid + ": " + str);
-                        // showSnackbar("명함 수신 완료");
+          String str = String.fromCharCodes(payload.bytes!);
+          // showSnackbar(endid + ": " + str);
+          // showSnackbar("명함 수신 완료");
 
-                        if (str.contains(':')) {
-                          // used for file payload as file payload is mapped as
-                          // payloadId:filename
-                          int payloadId = int.parse(str.split(':')[0]);
-                          String fileName = (str.split(':')[1]);
+          if (str.contains(':')) {
+            // used for file payload as file payload is mapped as
+            // payloadId:filename
+            int payloadId = int.parse(str.split(':')[0]);
+            String fileName = (str.split(':')[1]);
 
-                          if (map.containsKey(payloadId)) {
-                            if (tempFileUri != null) {
-                              moveFile(tempFileUri!, fileName);
-                            } else {
-                              // showSnackbar("File doesn't exist");
-                            }
-                          } else {
-                            //add to map if not already
-                            map[payloadId] = fileName;
-                          }
-                        }
-                      } else if (payload.type == PayloadType.FILE) {
-                        // showSnackbar(endid + ": File transfer started");
-                        tempFileUri = payload.uri;
-                      }
-                    },
-                    onPayloadTransferUpdate: (endid, payloadTransferUpdate) {
-                      if (payloadTransferUpdate.status ==
-                          PayloadStatus.IN_PROGRESS) {
-                        print(payloadTransferUpdate.bytesTransferred);
-                      } else if (payloadTransferUpdate.status ==
-                          PayloadStatus.FAILURE) {
-                        print("failed");
-                        showSnackbar(endid + ": FAILED to transfer file");
-                      } else if (payloadTransferUpdate.status ==
-                          PayloadStatus.SUCCESS) {
-                        // showSnackbar(
-                        //     "$endid success, total bytes = ${payloadTransferUpdate.totalBytes}");
-
-                        // if (map.containsKey(payloadTransferUpdate.id)) {
-                        //   //rename the file now
-                        //   String name = map[payloadTransferUpdate.id]!;
-                        //   moveFile(tempFileUri!, name);
-                        // } else {
-                        //   //bytes not received till yet
-                        //   map[payloadTransferUpdate.id] = "";
-                        // }
-                      }
-                    },
-                  );
-                },
-              ),
-              ElevatedButton(
-                child: Text("Reject Connection"),
-                onPressed: () async {
-                  Navigator.pop(context);
-                  try {
-                    await Nearby().rejectConnection(id);
-                  } catch (e) {
-                    showSnackbar(e);
-                  }
-                },
-              ),
-            ],
-          ),
-        );
+            if (map.containsKey(payloadId)) {
+              if (tempFileUri != null) {
+                moveFile(tempFileUri!, fileName);
+              } else {
+                // showSnackbar("File doesn't exist");
+              }
+            } else {
+              //add to map if not already
+              map[payloadId] = fileName;
+            }
+          }
+        }
       },
     );
   }
