a2822f648aeb1b6599b4eb3e5e4403de2f4e4f29
jocy0412 <jakob1@naver.com>
2022-07-22 13:58:10 +0900

    #33 연락처 UI 수정

--- lib/tests/contacts/preferences.dart
+++ lib/tests/contacts/preferences.dart
@@ -5,24 +5,23 @@ Map UserPreferences_db = {
     imagePath: 'http://34.64.217.3:3000/static/junglelover.gif',
     nickname: '정글러버',
     introduction: 'Pintos 정복자 😎',
-    tag: ['#코딩잘하나요', '#캠핑', '#베이킹'],
+    tag: ['코딩잘하나요', '캠핑', '베이킹'],
   ),
   '배그러버': User(
     imagePath: 'http://34.64.217.3:3000/static/bglover.png',
     nickname: '배그러버',
     introduction: '포친키 탄약도둑 😝',
     tag: [
-      '#배그',
-      '#애니',
-      '#커피',
-      '#코딩',
+      '배그',
+      '애니',
+      '커피',
     ],
   ),
   'Opjoobe': User(
     imagePath: 'http://34.64.217.3:3000/static/opjoobe.gif',
     nickname: 'Opjoobe',
     introduction: 'Ball is Life',
-    tag: ['#농구꽤하지', '#독서좋아요', '#다리우스', '#코딩천잽니다'],
+    tag: ['농구꽤하지', '독서좋아요', '다리우스'],
   ),
 };
 List Friends = ['정글러버', '배그러버', 'Opjoobe'];
