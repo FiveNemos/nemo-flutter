0645cbd2e2b038ba8fdb860cfddeffd3a0c8f491
latteishorse <moots_photo@naver.com>
2022-07-27 11:59:43 +0900

    #42 bottomNavigationBar 지도 추가

--- lib/screens/mypage/profile_page.dart
+++ lib/screens/mypage/profile_page.dart
@@ -154,6 +154,51 @@ class _ProfilePageState extends State<ProfilePage> {
           },
           separatorBuilder: (context, i) => SizedBox(height: 15),
         ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.contacts),
+              label: '연락처',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.share),
+              label: '공유',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              label: 'Map',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.message),
+              label: '메시지',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: '마이페이지',
+            ),
+          ],
+          currentIndex: 4,
+          onTap: (int index) {
+            switch (index) {
+              case 0:
+                Navigator.pushNamed(context, '/contacts');
+                break;
+              case 1:
+                Navigator.pushNamed(context, '/sharing');
+                break;
+              case 2:
+                Navigator.pushNamed(context, '/map');
+                break;
+              case 3:
+                Navigator.pushNamed(context, '/message');
+                break;
+              case 4:
+                // Navigator.pushNamed(context, '/mypage');
+                break;
+            }
+          },
+        ),
       );
     } else {
       return Center(
